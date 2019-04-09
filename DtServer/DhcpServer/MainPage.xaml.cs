c08503669c5f6825427e044dac542093ee562c01
Nicolò Dott. Ing. Buccheri <n.buccheri@outlook.com>
2019-04-09 10:56:23 +0200

    App

---
+++ DtServer/DhcpServer/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using DhcpServer.Model;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Net;
+using ServerTcp;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace DhcpServer
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private const int FLASH_TIME = 500;
+        private const int DELAY = 10000;
+        private const string RL = "\n";
+        private const bool LOOP_SCORE = true;
+        private const string DNS = "DomainIoT";
+        private const string SUB_MASK = "255.255.0.0";
+        private const string SERVER_IDENTIFIER = "169.254.139.40";
+        private const string ROUTER_IP = "169.254.139.40";
+        private const string AVVIA = "Avvia";
+        private const string UPDATE = "Aggiorna";
+
+        private static Server Server;
+
+        private Dhcp.DhcpServer DhcpServer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new UiBindingViewModel();
+            Start();
+        }
+
+        public UiBindingViewModel ViewModel { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Start();
+        }
+
+        private void Start()
+        {
+            if (this.StatusButton.Content.ToString().Equals(AVVIA))
+            {
+                this.ViewModel.Action = "AVVIO DEL SERVER DHCP IN CORSO...";
+                RunServer();
+                this.StatusButton.Content = UPDATE;
+                Task.Run(async () => //Task.Run automatically unwraps nested Task types!
+                {
+                    await Task.Delay(DELAY);
+                    Server = new Server();
+                    Server.StartServer();
+                });
+            }
+
+            var st = DhcpServer.Status;
+
+            if (!(st is null) && st.Count > 0)
+            {
+                foreach (var s in st)
+                {
+                    ViewModel.Action = s;
+                }
+
+                DhcpServer.Readed = true;
+                DhcpServer.UpdateReaded();
+            }
+
+            try
+            {
+                if (!(Server is null) && !(Server.Status is null) && Server.Status.Count > 0)
+                {
+                    foreach (var s in Server.Status)
+                    {
+                        ViewModel.Action = s;
+                    }
+
+                    Server.Status = new List<string>();
+                }
+            }
+            catch (Exception e)
+            {
+                ViewModel.Action = "\t->\tÈ STATA GENERATA UN'ECCEZIONE NEL SERVER DHCP";
+                ViewModel.Action = $"\t->\t{e.Message}";
+                ViewModel.Action = $"\t->\t{e.StackTrace}";
+            }
+        }
+
+        private void RunServer()
+        {
+            IPAddress iPAddress;
+
+            DhcpServer = new Dhcp.DhcpServer();
+            iPAddress = new IPAddress(new byte[] { 169, 254, 42, 91 });
+            DhcpServer.Run(iPAddress, DNS, SUB_MASK, SERVER_IDENTIFIER, ROUTER_IP);
+        }
+    }
+}
---
+++ DtServer/DhcpServer/Model/UiBinding.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DhcpServer.Model
+{
+    public class UiBinding
+    {
+        public string Score { get; set; }
+                
+        public string ScoreLine
+        {
+            get
+            {
+                return Score;
+            }
+        }
+    }
+    public class UiBindingViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private UiBinding defaultUiBinding = new UiBinding();
+        public UiBinding DefaultUiBinding { get { return this.defaultUiBinding; } }
+        private ObservableCollection<UiBinding> uiBindings = new ObservableCollection<UiBinding>();
+        public ObservableCollection<UiBinding> UiBindings { get { return this.uiBindings; } }
+        
+        public string Action
+        {
+            set
+            {
+                this.uiBindings.Add(new UiBinding()
+                {
+                    Score = value
+                });
+                this.OnPropertyChanged();
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
